20391c5ef55bb29ed735d096c0e0276f8c8f3e9b
rawa.shafaay <rawa.shafaay@globant.com>
2025-07-19 02:31:16 +0300

    create splash screen

--- lib/core/config/app_router.dart
+++ lib/core/config/app_router.dart
@@ -1,10 +1,14 @@
+import 'package:ecommerce/features/splash/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static const String splash = '/';
+  static const String login = '/login';
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
       default:
         return MaterialPageRoute(
           builder: (_) =>
